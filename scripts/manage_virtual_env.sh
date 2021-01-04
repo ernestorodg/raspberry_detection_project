65bf2c96b79143a12f940ecfb45d709123f7ccc6
Ernesto Rodriguez <ernestorg@poli.ufrj.br>
2021-01-04 19:21:08 -0300

    add tensorflow installation

--- scripts/manage_virtual_env.sh
+++ scripts/manage_virtual_env.sh
@@ -17,3 +17,7 @@ pip install opencv-contrib-python
 # For headless implementation:
 # pip install opencv-python-headless
 # pip install opencv-contrib-python-headless 
+
+# tensorflow will be installed using .whl file
+
+https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_armv7l.whl
